7e0b84968140e39b240c1675a68fe3f8848082ba
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2026-07-25 15:56:52 +0200

    array pipeline

--- vars/buildService.groovy
+++ vars/buildService.groovy
@@ -1,4 +1,20 @@
+/**
+ * High-level pipeline that supports multiple Python projects and Dockerfiles.
+ *
+ * Parameters:
+ *   pythonProjects  - List of maps: [path: '...', name: '...']
+ *   dockerProjects  - List of maps: [path: '...', name: '...']
+ *   context         - Docker build context (default: '.')
+ *   tag             - Image tag (default: BUILD_NUMBER)
+ *   extraArgs       - Extra args for Python steps
+ */
 def call(Map config = [:]) {
+
+    def pythonProjects = config.pythonProjects ?: []
+    def dockerProjects = config.dockerProjects ?: []
+    def context        = config.context        ?: '.'
+    def tag            = config.tag            ?: env.BUILD_NUMBER
+    def extraArgs      = config.extraArgs      ?: ''
 
     pipeline {
         agent {
@@ -12,29 +28,47 @@ def call(Map config = [:]) {
                 }
             }
 
+            // ---------- Python ----------
             stage('Python') {
                 when {
-                    expression { return config.pyprojectPath }
+                    expression { return pythonProjects }
                 }
                 steps {
-                    buildPython(
-                        pyprojectPath: config.pyprojectPath,
-                        extraArgs: config.extraArgs
-                    )
+                    script {
+                        pythonProjects.each { project ->
+                            def path = project.path
+                            def name = project.name ?: path
+
+                            echo "=== Python project: ${name} (${path}) ==="
+                            buildPython(
+                                pyprojectPath: path,
+                                extraArgs: extraArgs
+                            )
+                        }
+                    }
                 }
             }
 
+            // ---------- Docker ----------
             stage('Docker') {
                 when {
-                    expression { return config.dockerfilePath }
+                    expression { return dockerProjects }
                 }
                 steps {
-                    buildDocker(
-                        dockerfilePath: config.dockerfilePath,
-                        imageName: config.imageName,
-                        context: config.context,
-                        tag: config.tag
-                    )
+                    script {
+                        dockerProjects.each { project ->
+                            def path = project.path
+                            def name = project.name ?: env.JOB_NAME.replaceAll('/', '-')
+
+                            echo "=== Docker image: ${name} (${path}) ==="
+                            buildDocker(
+                                dockerfilePath: path,
+                                imageName: name,
+                                context: context,
+                                tag: tag
+                            )
+                        }
+                    }
                 }
             }
         }
